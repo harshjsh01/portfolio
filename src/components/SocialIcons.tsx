d92e49d727fed98a2e56bef818ed3e71b8b936ec
harshjsh01 <harshjsh02@gmail.com>
2026-07-23 22:37:00 +0530

    Update social links

--- src/components/SocialIcons.tsx
+++ src/components/SocialIcons.tsx
@@ -58,12 +58,12 @@ const SocialIcons = () => {
     <div className="icons-section">
       <div className="social-icons" data-cursor="icons" id="social">
         <span>
-          <a href="https://github.com" target="_blank">
+          <a href="https://github.com/harshjsh01" target="_blank" rel="noopener noreferrer">
             <FaGithub />
           </a>
         </span>
         <span>
-          <a href="https://www.linkedin.com" target="_blank">
+          <a href="https://www.linkedin.com/in/harshjsh01" target="_blank" rel="noopener noreferrer">
             <FaLinkedinIn />
           </a>
         </span>
